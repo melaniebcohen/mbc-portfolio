416245f8ce9909a10b159c8248e186b029ec7ddb
Melanie Cohen <melaniebcohen@gmail.com>
2018-11-02 21:16:38 -0700

    styling changes

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,8 +2,8 @@ import React, { Component, Fragment } from 'react';
 import './_hero.scss';
 
 const Hero = () => {
-  return <section>
-  {/* return <section className='hero'> */}
+  // return <section>
+  return <section className='hero'>
     {/* <div id='hero-center'>
       <h1>Melanie Cohen</h1>
       <h2>Front-End Developer</h2>
