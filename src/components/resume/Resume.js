80d380d5c3c1214d203dbd86172d62e1b74d519b
Melanie Cohen <melaniebcohen@gmail.com>
2019-09-27 15:40:01 -0700

    updating resume component

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -4,39 +4,101 @@ import './_resume.scss';
 const Resume = ({ activeComponent }) => {
   if (activeComponent === 'Resume') {
     return <section className='resume' style={{ paddingBottom: '3rem' }}>
-      <div>
-        <h3>Current</h3>
-        <div>
-          <img src="../../assets/logos/code-fellows.png"></img>
-          <h4>Code Fellows TA</h4>
+      <h3>Current</h3>
+      <div className="item">
+        <div className="item-img-container">
+          <img src="../../assets/logos/mc-freelance.png"></img>
         </div>
-        <div>
+        <div className="item-text">
           <h4>Freelance</h4>
+          <p>January 2019 - August 2019</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
         </div>
       </div>
-      <div>
-        <h3>Previous</h3>
-        <div>
+      <div className="item">
+        <div className="item-img-container">
+          <img src="../../assets/logos/codefellows.png"></img>
+        </div>
+        <div className="item-text">
+          <h4>Code Fellows Teaching Assistant</h4>
+          <p>October 2019 - Present</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <h3>Previous</h3>
+      <div className="item">
+        <div className="item-img-container">
           <img src="../../assets/logos/drift.png"></img>
-          <h4>Drift</h4>
         </div>
-        <div>
+        <div className="item-text">
+          <h4>Drift Marketplace</h4>
+          <p>January 2019 - August 2019</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="item">
+        <div className="item-img-container">
           <img src="../../assets/logos/csm-pacific.png"></img>
+        </div>
+        <div className="item-text">
           <h4>CSM Pacific</h4>
+          <p>January 2019 - August 2019</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
         </div>
-        <div>
+      </div>
+      <div className="item">
+        <div className="item-img-container">
           <img src="../../assets/logos/carena.png"></img>
-          <h4>Carena</h4>
         </div>
-        <div>
+        <div className="item-text">
+          <h4>Carena, Inc.</h4>
+          <p>January 2019 - August 2019</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="item">
+        <div className="item-img-container">
           <img src="../../assets/logos/apple.png"></img>
-          <h4>Apple</h4>
+        </div>
+        <div className="item-text">
+          <h4>Apple, Inc.</h4>
+          <p>January 2019 - August 2019</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </li>
+          </ul>
         </div>
       </div>
-      <div>
-        <h3>Education</h3>
-      </div>
-      <div>
+      <h3>Education</h3>
+      <div className="item">
         <h3>Extra Stuff</h3>
       </div>
     </section>;
